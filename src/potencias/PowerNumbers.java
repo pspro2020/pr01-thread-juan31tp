5b2ddb49e6c229f66b5877b3058f5bffb84fb79d
Juan Antonio Tejada <juan31tp@gmail.com>
2020-10-02 18:05:55 +0200

    Exercise Done!

---
+++ src/potencias/PowerNumbers.java
@@ -0,0 +1,19 @@
+package potencias;
+
+import java.lang.Thread;
+
+public class PowerNumbers extends Thread {
+
+    private final int number;
+
+    PowerNumbers(int number) {
+        this.number=number;
+    }
+
+    @Override
+    public void run(){
+        for (int i=1; i<=10; i++){
+            System.out.printf("%d ^ %d = %f\n ", number, i, Math.pow(number, i));
+        }
+    }
+}
